91e69a9ea43eec64ba9cfd256e10e26fa65f753f
nhasjim <nhasjim@gmail.com>
2017-05-03 13:32:43 -0700

    got sample flickr photos working

--- js/jsonTest.js
+++ js/jsonTest.js
@@ -1,13 +1,28 @@
-var url = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=65030e1f766ba9dccb6deb836165ca4a&user_id=48659894@N03";
-
+var apiurl = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=65030e1f766ba9dccb6deb836165ca4a&user_id=48659894@N03";
+var imageUrl;
+var image;
 console.log("hello");
 
 $(document).ready(function () {
   $('#get-data').click(function () {
     var showData = $('#show-data');
-    $.getJSON(url + "&format=json&jsoncallback=?", function (data) { //need that &jsoncallback
+    $.getJSON(apiurl + "&format=json&jsoncallback=?", function (data) { //need that &jsoncallback
       console.log(data);
-      showData.text(data)
+      console.log(data.photos.photo.length);
+
+      $.each(data.photos.photo, function(index, value){
+        console.log("index: " + index + "value.farm: " + value.farm);
+        imageUrl = "http://farm" + data.photos.photo[index].farm + ".static.flickr.com/" + data.photos.photo[index].server + "/"+data.photos.photo[index].id + "_"+data.photos.photo[index].secret + ".jpg";
+        console.log(image);
+        var image = document.createElement("img");
+        image.src = imageUrl;
+
+        //document.getElementById("show-data").appendChild(elem);
+        showData.append(image); //does same as above
+
+
+      });
+
     });
   });
 });
