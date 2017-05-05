6599d34cea22a7a00dda334487b1c70529ce9c1c
nhasjim <nhasjim@gmail.com>
2017-05-05 14:16:29 -0700

    lol

--- js/jsonTest.js
+++ js/jsonTest.js
@@ -1,4 +1,4 @@
-var apiurl = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=65030e1f766ba9dccb6deb836165ca4a&user_id=48659894@N03";
+var apiurl = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=65030e1f766ba9dccb6deb836165ca4a&user_id=129194286@N08&format=json";
 var imageUrl;
 var image;
 console.log("hello");
@@ -6,14 +6,12 @@ console.log("hello");
 $(document).ready(function () {
   $('#get-data').click(function () {
     var showData = $('#show-data');
-    $.getJSON(apiurl + "&format=json&jsoncallback=?", function (data) { //need that &jsoncallback
+    $.getJSON(apiurl + "&jsoncallback=?", function (data) { //need that &jsoncallback
       console.log(data);
       console.log(data.photos.photo.length);
 
       $.each(data.photos.photo, function(index, value){
-        console.log("index: " + index + "value.farm: " + value.farm);
         imageUrl = "http://farm" + data.photos.photo[index].farm + ".static.flickr.com/" + data.photos.photo[index].server + "/"+data.photos.photo[index].id + "_"+data.photos.photo[index].secret + ".jpg";
-        console.log(image);
         var image = document.createElement("img");
         image.src = imageUrl;
 
